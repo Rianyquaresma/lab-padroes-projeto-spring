0976464fef34a2570debc3d6fe5ef64a744e9595
Riany <ryquaresmah1106@gmail.com>
2025-04-04 20:23:25 -0300

    first commit

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinovation/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,23 @@
+package one.digitalinovation.lab_padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * Spring Data JPA
+ * Spring Web
+ * H2 Database
+ * OpenFeing
+ *
+ * @autor: RianyQuaresma
+*/
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
